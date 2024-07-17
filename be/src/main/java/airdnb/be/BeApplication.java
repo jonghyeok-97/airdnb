ac72c9892bd4336aa68f1fd85d8c040bea7560e4
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-07-17 22:41:20 +0900

    style: 패키지 이동

--- be/src/main/java/airdnb/be/BeApplication.java
+++ be/src/main/java/airdnb/be/BeApplication.java
@@ -1,4 +1,4 @@
-package airdnb.demo;
+package airdnb.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- be/src/test/java/airdnb/be/BeApplicationTests.java
+++ be/src/test/java/airdnb/be/BeApplicationTests.java
@@ -1,4 +1,4 @@
-package airdnb.demo;
+package airdnb.be;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
