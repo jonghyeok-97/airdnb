a3ea034eb91ddea7347fd70545a8ea9d02e8eade
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-07-27 23:23:24 +0900

    [feat/#2] feat: 이메일 config 추가

---
+++ be/src/main/java/airdnb/be/config/EmailConfig.java
@@ -0,0 +1,41 @@
+package airdnb.be.config;
+
+import java.util.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost("smpt.naver.com");
+        javaMailSender.setPort(465);
+        javaMailSender.setUsername(env.getProperty("spring.mail.username"));
+        javaMailSender.setPassword(env.getProperty("spring.mail.password"));
+        javaMailSender.setJavaMailProperties(getEnvProperties());
+
+        return javaMailSender;
+    }
+
+    private Properties getEnvProperties() {
+        Properties properties = new Properties();
+
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.ssl.trust", "smpt.naver.com");
+        properties.setProperty("mail.smtp.ssl.enable", "true");
+
+        return properties;
+    }
+}
