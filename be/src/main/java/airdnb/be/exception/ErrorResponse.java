09f6030ddaeb435a4c515872ab9518bb576ef607
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-08-02 18:26:03 +0900

    [feat/#2] feat: 비지니스 예외 처리 추가

--- be/src/main/java/airdnb/be/exception/ErrorResponse.java
+++ be/src/main/java/airdnb/be/exception/ErrorResponse.java
@@ -1,9 +1,22 @@
 package airdnb.be.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 
-public record ErrorResponse(
-        HttpStatus status,
-        String field,
+public record ErrorResponse (
+        String code,
+        String status,
         String message
-) {}
+){
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode.getCode(), errorCode.getStatus().name(), errorCode.getMessage());
+    }
+
+    public static ErrorResponse of(HttpStatus httpStatus, FieldError fieldError) {
+        String code = String.format("0%s", httpStatus.value());
+        String status = httpStatus.name();
+        String message = String.format("%s 검증 실패입니다. (%s)", fieldError.getField(), fieldError.getRejectedValue());
+
+        return new ErrorResponse(code, status, message);
+    }
+}
--- be/src/main/java/airdnb/be/exception/ExceptionHandlers.java
+++ be/src/main/java/airdnb/be/exception/ExceptionHandlers.java
@@ -1,7 +1,9 @@
 package airdnb.be.exception;
 
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,15 +20,14 @@ public class ExceptionHandlers {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValidEx(MethodArgumentNotValidException ex) {
-        FieldError fieldError = ex.getFieldError();
-        return new ErrorResponse(HttpStatus.BAD_REQUEST, getField(fieldError), getMessage(fieldError));
+        FieldError fieldError = Objects.requireNonNull(ex.getFieldError());
+
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST, fieldError);
     }
 
-    private String getField(FieldError fieldError) {
-        return fieldError.getField();
-    }
-
-    private String getMessage(FieldError fieldError) {
-        return fieldError.getDefaultMessage();
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessEx(BusinessException ex) {
+        return ResponseEntity.status(ex.getErrorCode().getStatus())
+                .body(ErrorResponse.of(ex.getErrorCode()));
     }
 }
