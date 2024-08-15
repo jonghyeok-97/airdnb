5525ad44351e8c0b8688c5c796718b46ab637555
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-08-15 17:27:00 +0900

    [feat/#11] feat: 외부 메일서버에 메일을 못 보낼 시 재시도 로직 추가

--- be/src/main/java/airdnb/be/BeApplication.java
+++ be/src/main/java/airdnb/be/BeApplication.java
@@ -2,7 +2,9 @@ package airdnb.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class BeApplication {
 
