b7b02f4c7a2cb6dd350452b235b186b7a7cd2fe5
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-08-16 14:27:36 +0900

    [feat/#13] feat: 멤버의 email에 유니크 추가 & 컬럼에 nullable 추가

--- be/src/main/java/airdnb/be/domain/member/entitiy/Member.java
+++ be/src/main/java/airdnb/be/domain/member/entitiy/Member.java
@@ -1,14 +1,18 @@
 package airdnb.be.domain.member.entitiy;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email", name = "unique_email_constraints"))
 public class Member {
 
     @Id
@@ -17,10 +21,13 @@ public class Member {
 
     private String name;
 
+    @Column(nullable = false)
     private String email;
 
+    @Column(length = 13)
     private String phoneNumber;
 
+    @Column(nullable = false)
     private String password;
 
     public Member(String name, String email, String phoneNumber, String password) {
