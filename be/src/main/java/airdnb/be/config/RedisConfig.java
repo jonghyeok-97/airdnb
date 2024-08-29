d1e045ffa386ea5fd87168cdc68fdc652902bd8a
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-08-29 16:30:31 +0900

    [feat/#17] refactor: 레디스 관련 빈들을 인터페이스를 반환하도록 변경

--- be/src/main/java/airdnb/be/config/RedisConfig.java
+++ be/src/main/java/airdnb/be/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
@@ -11,7 +12,7 @@ public class RedisConfig {
 
     // 레디스 서버와 비동기로 연결
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
+    public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory();
     }
 
@@ -20,7 +21,7 @@ public class RedisConfig {
      * @return 문자열(UUID)에 최적화된 직렬화를 선택하도록 지정
      */
     @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisOperations<String, String> redisOperations(RedisConnectionFactory redisConnectionFactory) {
         StringRedisTemplate template = new StringRedisTemplate();
         template.setConnectionFactory(redisConnectionFactory);
         return template;
