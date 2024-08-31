ae6afa5176940ef2c50d7bf4cd6f9a43c3894017
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-08-31 21:49:17 +0900

    [feat/#18] refactor: 회원의 비밀번호 일치하는 테스트를 parameterizedTest로 합침

--- be/src/test/java/airdnb/be/domain/member/entitiy/MemberTest.java
+++ be/src/test/java/airdnb/be/domain/member/entitiy/MemberTest.java
@@ -3,33 +3,22 @@ package airdnb.be.domain.member.entitiy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class MemberTest {
 
-    @DisplayName("회원은 비밀번호가 일치하면 True 이다.")
-    @Test
-    void hasPassword() {
+    @DisplayName("회원은 비밀번호가 일치하는지 확인할 수 있다.")
+    @ParameterizedTest
+    @CsvSource({"password, true", "password1, false"})
+    void hasPassword(String password, boolean expected) {
         // given
         Member member = new Member("이름1", "1@naver.com", "010-1111-1111", "password");
 
         // when
-        boolean result = member.hasPassword("password");
+        boolean result = member.hasPassword(password);
 
         // then
-        assertThat(result).isTrue();
-    }
-
-    @DisplayName("회원은 비밀번호가 일치하지 않으면 false 이다.")
-    @Test
-    void hasNotPassword() {
-        // given
-        Member member = new Member("이름1", "1@naver.com", "010-1111-1111", "password");
-
-        // when
-        boolean result = member.hasPassword("password123");
-
-        // then
-        assertThat(result).isFalse();
+        assertThat(result).isEqualTo(expected);
     }
 }
