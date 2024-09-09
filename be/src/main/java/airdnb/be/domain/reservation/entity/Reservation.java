4ba63f9f59544dd674e362ae9bcffcc8045071b4
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-09-09 14:44:03 +0900

    [feat/#21] feat: 예약 엔티티 추가

--- be/src/main/java/airdnb/be/domain/reservation/entity/Reservation.java
+++ be/src/main/java/airdnb/be/domain/reservation/entity/Reservation.java
@@ -1,18 +1,41 @@
-package airdnb.be.reservation;
+package airdnb.be.domain.reservation.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Entity
 public class Reservation {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long reservationId;
+
+    @Column(nullable = false)
     private Long stayId;
+
+    @Column(nullable = false)
     private Long guestId;
+
+    @Column(nullable = false)
     private LocalDateTime checkIn;
+
+    @Column(nullable = false)
     private LocalDateTime checkOut;
+
+    @Column(nullable = false)
     private int guestCount;
+
+    @Column(nullable = false)
     private BigDecimal totalFee;
 
     public Reservation(Long stayId, Long guestId, LocalDateTime checkIn, LocalDateTime checkOut,
