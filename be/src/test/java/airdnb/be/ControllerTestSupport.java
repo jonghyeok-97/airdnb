14d6f2ecf07578a0a31a0b293c299e6a4752d202
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-09-11 16:54:54 +0900

    [feat/#21] feat: 예약 Request 빈 Validation추가

--- be/src/test/java/airdnb/be/ControllerTestSupport.java
+++ be/src/test/java/airdnb/be/ControllerTestSupport.java
@@ -2,8 +2,10 @@ package airdnb.be;
 
 import airdnb.be.domain.mail.MailService;
 import airdnb.be.domain.member.service.MemberService;
+import airdnb.be.domain.reservation.service.ReservationService;
 import airdnb.be.domain.stay.service.StayService;
 import airdnb.be.web.member.MemberController;
+import airdnb.be.web.reservation.ReservationController;
 import airdnb.be.web.stay.StayController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +14,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = {
-       MemberController.class,
-       StayController.class
+        MemberController.class,
+        StayController.class,
+        ReservationController.class
 })
 public abstract class ControllerTestSupport {
 
@@ -31,4 +34,7 @@ public abstract class ControllerTestSupport {
 
     @MockBean
     protected StayService stayService;
+
+    @MockBean
+    protected ReservationService reservationService;
 }
