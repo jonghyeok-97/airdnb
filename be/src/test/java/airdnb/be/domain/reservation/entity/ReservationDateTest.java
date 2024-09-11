73f0f73ac3a1deb183ffb60170e70470137f816f
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-09-11 15:31:53 +0900

    [feat/#21] test: 예약날짜 동등성 테스트 추가

--- be/src/test/java/airdnb/be/domain/reservation/entity/ReservationDateTest.java
+++ be/src/test/java/airdnb/be/domain/reservation/entity/ReservationDateTest.java
@@ -20,4 +20,37 @@ class ReservationDateTest {
         // then
         assertThat(status).isEqualTo(ReservationStatus.RESERVED);
     }
+
+    @DisplayName("숙소ID와 예약 날짜가 같으면 동등하다.")
+    @Test
+    void equalsAndHashCodeByStayIdAndDate() {
+        // given
+        ReservationDate reservationDate1 = new ReservationDate(1L, LocalDate.of(2024, 4, 5));
+        ReservationDate reservationDate2 = new ReservationDate(1L, LocalDate.of(2024, 4, 5));
+
+        // when then
+        assertThat(reservationDate1).isEqualTo(reservationDate2);
+    }
+
+    @DisplayName("예약 날짜가 같지 않으면 동등하지 않다.")
+    @Test
+    void equalsAndHashCodeByStayIdAndDate2() {
+        // given
+        ReservationDate reservationDate1 = new ReservationDate(1L, LocalDate.of(2024, 4, 5));
+        ReservationDate reservationDate2 = new ReservationDate(1L, LocalDate.of(2024, 4, 6));
+
+        // when then
+        assertThat(reservationDate1).isNotEqualTo(reservationDate2);
+    }
+
+    @DisplayName("숙소ID가 같지 않으면 동등하지 않다.")
+    @Test
+    void equalsAndHashCodeByStayIdAndDate3() {
+        // given
+        ReservationDate reservationDate1 = new ReservationDate(1L, LocalDate.of(2024, 4, 5));
+        ReservationDate reservationDate2 = new ReservationDate(2L, LocalDate.of(2024, 4, 5));
+
+        // when then
+        assertThat(reservationDate1).isNotEqualTo(reservationDate2);
+    }
 }
