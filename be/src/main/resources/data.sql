cd9e93efa91ad00e52f6a683325c50f15cb1d36e
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-09-13 19:19:54 +0900

    [feat/#23] style: http 파일 추가 및 data.sql 수정

--- be/src/main/resources/data.sql
+++ be/src/main/resources/data.sql
@@ -1,6 +1,5 @@
 insert into member(phone_number, email, name, password)
     values ('010-1234-1234', '1@naver.com', '1', '1');
 
-# 위도(-90~90, y) 경도(-180 ~ 180, x) 순
- insert into stay(check_in_time, check_out_time, fee_per_night, guest_count, member_id, title, point)
+insert into stay(check_in_time, check_out_time, fee_per_night, guest_count, host_id, title, point)
      values ('19:00:00', '12:00:00', 20000.00, 3, 1, '제목2', ST_GeomFromText('POINT(37.9780 128.5665)', 4326));
