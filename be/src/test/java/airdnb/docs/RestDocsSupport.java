5b3c8373621752a5565be7cecdf28564a15e6afc
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-09-13 15:26:14 +0900

    [feat/#23] test: docs용 숙소 등록 API 테스트 추가

--- be/src/test/java/airdnb/docs/RestDocsSupport.java
+++ be/src/test/java/airdnb/docs/RestDocsSupport.java
@@ -2,6 +2,9 @@ package airdnb.docs;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.restdocs.RestDocumentationContextProvider;
@@ -13,6 +16,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 public abstract class RestDocsSupport {
 
     protected MockMvc mockMvc;
+    protected ObjectMapper objectMapper = initObjectMapper();
 
     @BeforeEach
     void setUp(RestDocumentationContextProvider provider) {
@@ -22,4 +26,11 @@ public abstract class RestDocsSupport {
     }
 
     protected abstract Object initController();
+
+    private ObjectMapper initObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
 }
