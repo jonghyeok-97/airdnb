e18be0c699294194e980007a9e28962a615b45f5
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-09-13 14:14:07 +0900

    [feat/#23] feat: docs를 만들 베이스 클래스 추가

---
+++ be/src/test/java/airdnb/docs/RestDocsSupport.java
@@ -0,0 +1,25 @@
+package airdnb.docs;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class RestDocsSupport {
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(initController()) // 문서를 띄울 때는 서버를 띄우지 않기 위함
+                .apply(documentationConfiguration(provider))
+                .build();
+    }
+
+    protected abstract Object initController();
+}
