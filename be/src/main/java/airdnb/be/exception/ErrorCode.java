5c1f35ae7291296fa12a41f062795fb6f6f356f2
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-09-14 21:13:48 +0900

    [feat/#24] feat: 로그인이 필요한 ErrorCode 추가

--- be/src/main/java/airdnb/be/exception/ErrorCode.java
+++ be/src/main/java/airdnb/be/exception/ErrorCode.java
@@ -10,11 +10,11 @@ public enum ErrorCode {
 
     // 회원
     ALREADY_EXISTS_MEMBER("1000", HttpStatus.BAD_REQUEST, "이미 존재하는 회원입니다."),
-    CANNOT_LOGIN("1001", HttpStatus.BAD_REQUEST, "아이디나 비밀번호가 틀렸습니다"),
     NOT_EXIST_MEMBER("1002", HttpStatus.BAD_REQUEST, "존재하지 않는 회원입니다"),
 
     // 인증
-    AUTH_MISMATCH("1100", HttpStatus.BAD_REQUEST, "인증 되지 않았습니다"),
+    CANNOT_LOGIN("1100", HttpStatus.BAD_REQUEST, "아이디나 비밀번호가 틀렸습니다"),
+    NOT_LOGIN("1101", HttpStatus.BAD_REQUEST,"로그인이 필요합니다"),
 
     // 숙소
     NOT_EXIST_STAY("1200", HttpStatus.BAD_REQUEST, "존재하지 않는 숙소입니다."),
@@ -24,7 +24,8 @@ public enum ErrorCode {
     ALREADY_EXISTS_RESERVATION("1300", HttpStatus.BAD_REQUEST, "이미 존재하는 예약입니다."),
 
     // 외부 API
-    MAIL_SERVER_ERROR("2000", HttpStatus.INTERNAL_SERVER_ERROR, "내부 오류입니다. 이용에 불편을 드려서 죄송합니다");
+    MAIL_SERVER_ERROR("2000", HttpStatus.INTERNAL_SERVER_ERROR, "외부 메일 서버 오류입니다."),
+    ;
 
     private final String code;
     private final HttpStatus status;
