874942c61f01ad7c02a1026a7dda6aa0639b223b
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-09-15 12:24:25 +0900

    [feat/#24] refactor: 테스트에 Session 속성 추가

--- be/src/main/java/airdnb/be/web/reservation/ReservationController.java
+++ be/src/main/java/airdnb/be/web/reservation/ReservationController.java
@@ -1,5 +1,6 @@
 package airdnb.be.web.reservation;
 
+import airdnb.be.annotation.argumentResolver.Login;
 import airdnb.be.domain.reservation.service.ReservationService;
 import airdnb.be.domain.reservation.service.response.ReservationResponse;
 import airdnb.be.web.ApiResponse;
@@ -17,8 +18,10 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @PostMapping("/reservation")
-    public ApiResponse<ReservationResponse> reserve(@RequestBody @Valid ReservationAddRequest request) {
-        ReservationResponse response = reservationService.reserve(request.toServiceRequest());
+    public ApiResponse<ReservationResponse> reserve(
+            @Login Long userId,
+            @RequestBody @Valid ReservationAddRequest request) {
+        ReservationResponse response = reservationService.reserve(request.toServiceRequest(userId));
         return ApiResponse.ok(response);
     }
 }
