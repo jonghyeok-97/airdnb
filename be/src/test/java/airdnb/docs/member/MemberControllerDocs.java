e76e988010b50e958dbd83e2df6ffac55558628b
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-09-23 14:35:47 +0900

    [feat/#23] feat: 회원 가입시 이메일 인증 API 문서 추가

--- be/src/test/java/airdnb/docs/member/MemberControllerDocs.java
+++ be/src/test/java/airdnb/docs/member/MemberControllerDocs.java
@@ -1,23 +1,29 @@
 package airdnb.docs.member;
 
+import static airdnb.be.utils.SessionConst.MAIL_VERIFIED_MEMBER;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 
 import airdnb.be.domain.mail.MailService;
 import airdnb.be.domain.member.service.MemberService;
 import airdnb.be.web.member.MemberController;
+import airdnb.be.web.member.request.EmailAuthenticationRequest;
 import airdnb.be.web.member.request.EmailRequest;
 import airdnb.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 public class MemberControllerDocs extends RestDocsSupport {
 
@@ -71,6 +77,64 @@ public class MemberControllerDocs extends RestDocsSupport {
                 .andExpect(jsonPath("$.data").doesNotExist())
                 .andDo(document("member-exist-204",
                         preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint())));;
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @DisplayName("이메일 인증 성공 API")
+    @Test
+    void authenticateEmail() throws Exception {
+        // given
+        String email = "123@naver.com";
+        String authenticationCode = "312nj5acz";
+        EmailAuthenticationRequest request = new EmailAuthenticationRequest(email, authenticationCode);
+
+        given(mailService.isValidMail(request.toServiceRequest()))
+                .willReturn(true);
+
+        // when then
+        mockMvc.perform(
+                        get("/member/email/authenticate")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(jsonPath("$.code").value("0200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("OK"))
+                .andExpect(jsonPath("$.data").doesNotExist())
+                .andExpect(request().sessionAttribute(MAIL_VERIFIED_MEMBER, true))
+                .andDo(document("member-email-authenticate-200",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("회원 메일"),
+                                fieldWithPath("authCode").type(JsonFieldType.STRING).description("메일 인증 코드")
+                        )));
+    }
+
+    @DisplayName("이메일 인증 실패 API")
+    @Test
+    void authenticateEmailFail() throws Exception {
+        // given
+        String email = "123@naver.com";
+        String authenticationCode = "312njacz";
+        EmailAuthenticationRequest request = new EmailAuthenticationRequest(email, authenticationCode);
+
+        given(mailService.isValidMail(request.toServiceRequest()))
+                .willReturn(false);
+
+        // when then
+        mockMvc.perform(
+                        get("/member/email/authenticate")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(jsonPath("$.code").value("0400"))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.data").doesNotExist())
+                .andExpect(request().sessionAttributeDoesNotExist(MAIL_VERIFIED_MEMBER))
+                .andDo(document("member-email-authenticate-400",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
     }
 }
