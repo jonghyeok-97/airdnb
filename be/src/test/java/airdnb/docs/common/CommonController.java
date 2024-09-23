a3374fb398eb41147fae8b91274c00a61ec069c1
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-09-23 12:41:02 +0900

    [feat/#23] feat: 에러 응답 API 추가

--- be/src/test/java/airdnb/docs/common/CommonController.java
+++ be/src/test/java/airdnb/docs/common/CommonController.java
@@ -1,6 +1,9 @@
 package airdnb.docs.common;
 
+import airdnb.be.exception.ErrorCode;
 import airdnb.be.web.ApiResponse;
+import java.util.List;
+import java.util.stream.Stream;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommonController {
 
     @GetMapping("/docs")
-    public ApiResponse<Void> docs(){
+    public ApiResponse<Void> docsCommon(){
         return ApiResponse.ok();
+    }
+
+    @GetMapping("/error")
+    public List<ApiResponse<Object>> docsError(){
+        return Stream.of(ErrorCode.values())
+                .map(errorCode -> ApiResponse.errorCode(errorCode))
+                .toList();
     }
 }
