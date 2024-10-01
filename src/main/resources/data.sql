2dd6a0049fc5345bb2772e65e4b640736977af2a
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-10-01 19:28:14 +0900

    [feat/#32] data.sql에 예약 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 insert into member(phone_number, email, name, password)
-    values ('010-1234-1234', '1@naver.com', '1', '1');
+values ('010-1234-1234', '1@naver.com', '1', '1');
 
 insert into stay(check_in_time, check_out_time, fee_per_night, guest_count, host_id, title, point)
-     values ('19:00:00', '12:00:00', 20000.00, 3, 1, '제목2', ST_GeomFromText('POINT(37.9780 128.5665)', 4326));
+values ('15:00:00', '11:00:00', 20000.00, 3, 1, '제목2', ST_GeomFromText('POINT(37.9780 128.5665)', 4326));
+
+insert into reservation(guest_count, total_fee, check_in, check_out, guest_id, stay_id, status)
+VALUES (1, 10000.00, '2024-5-30 15:00:00', '2024-6-1 11:00:00', 1, 1, 'RESERVED')
