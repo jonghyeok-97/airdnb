16cc9dcbe4063808ac72b838f740ee969fde9d14
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-10-14 17:38:33 +0900

    [feat/#39] feat: 레디스에 데이터 추가 및 확인에 대한 테스트 추가

--- src/test/java/airdnb/be/client/RedisClientTest.java
+++ src/test/java/airdnb/be/client/RedisClientTest.java
@@ -1,10 +1,13 @@
 package airdnb.be.client;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import airdnb.be.IntegrationTestSupport;
 import java.util.concurrent.TimeUnit;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -16,27 +19,40 @@ class RedisClientTest extends IntegrationTestSupport {
     @Autowired
     private RedisClient redisClient;
 
-    @DisplayName("레디스")
+    @DisplayName("레디스에 데이터를 추가한다")
     @Test
-    void test() {
+    void addData() {
         // given
-        redisTemplate.opsForValue().set("124356", "1");
+        redisClient.addData("key", "value", 5L, TimeUnit.SECONDS);
+
         // when
+        String result = redisTemplate.opsForValue().get("key");
 
-        Boolean b = redisTemplate.hasKey("124356");
-        Assertions.assertThat(b).isTrue();
         // then
-
+        assertThat(result).isEqualTo("value");
     }
 
-    @DisplayName("")
+    @DisplayName("레디스에 키와 값이 있으면 True 를 반환한다")
     @Test
-    void test1() {
+    void hasDataIsTrue() {
         // given
         redisClient.addData("123", "d@naver.com", 5L, TimeUnit.MINUTES);
-        // when
-        Assertions.assertThat(redisClient.hasData("123", "d@naver.com")).isTrue();
-        // then
 
+        // when then
+        assertThat(redisClient.hasData("123", "d@naver.com")).isTrue();
+    }
+
+    @DisplayName("레디스에 키와 값중 하나라도 없으면 False 를 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {"key1,value,false", "key,value1,false"})
+    void hasDataIsFalse(String key, String value, boolean result) {
+        // given
+        redisClient.addData("key", "value", 5L, TimeUnit.MINUTES);
+
+        // when
+        boolean actual = redisClient.hasData(key, value);
+
+        // then
+        assertThat(actual).isEqualTo(result);
     }
 }
