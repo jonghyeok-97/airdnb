a987393d37424c6a3ad3e5830f1efbcac95fb416
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-10-14 17:29:36 +0900

    [feat/#39] feat: 해당 메일로 인증번호 1번 보내는 테스트 추가

---
+++ src/test/java/airdnb/be/client/MailClientTest.java
@@ -0,0 +1,32 @@
+package airdnb.be.client;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.verify;
+import static org.mockito.Mockito.times;
+
+import airdnb.be.IntegrationTestSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+class MailClientTest extends IntegrationTestSupport {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @DisplayName("해당 이메일로 인증번호를 1번 보낸다")
+    @Test
+    void sendAuthenticationMail() {
+        // given when
+        mailClient.sendAuthenticationMail("test@naver.com", "13mxken34");
+
+        // then
+        verify(javaMailSender, times(1)).send(any(SimpleMailMessage.class));
+    }
+}
