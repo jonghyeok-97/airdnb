c033f989a59dedb3e9d45061595cf210ce2ca264
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-10-20 17:16:38 +0900

    [feat/#35] style: 결제 승인 API 에 대한 .http 작성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,4 @@ insert into stay(check_in_time, check_out_time, fee_per_night, guest_count, host
 values ('15:00:00', '11:00:00', 20000.00, 3, 1, '제목2', ST_GeomFromText('POINT(37.9780 128.5665)', 4326));
 
 insert into reservation(guest_count, total_fee, check_in, check_out, guest_id, stay_id, status)
-VALUES (1, 10000.00, '2024-5-30 15:00:00', '2024-6-1 11:00:00', 1, 1, 'RESERVED')
+VALUES (1, 50000.00, '2024-5-30 15:00:00', '2024-6-1 11:00:00', 1, 1, 'RESERVED')
