5315ddabc875812412730fc81eb3ff7076bb3e3d
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-10-23 14:43:53 +0900

    [feat/#35] feat: 예약 결제 승인 컨트롤러 구현

--- src/main/java/airdnb/be/web/payment/PaymentController.java
+++ src/main/java/airdnb/be/web/payment/PaymentController.java
@@ -2,12 +2,14 @@ package airdnb.be.web.payment;
 
 import airdnb.be.annotation.argumentResolver.Login;
 import airdnb.be.domain.payment.service.PaymentFacade;
+import airdnb.be.domain.payment.service.response.PaymentReservationResponse;
 import airdnb.be.web.ApiResponse;
 import airdnb.be.web.payment.request.PaymentConfirmRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +29,15 @@ public class PaymentController {
         return ApiResponse.ok(
                 paymentFacade.addPaymentTemporaryData(memberId, reservationId, paymentKey, amount, orderId)
         );
+    }
+
+    @PostMapping("/payment/{paymentTemporaryId}/confirm")
+    public ApiResponse<PaymentReservationResponse> confirmPaymentByReservation(@Login Long memberId,
+                                                                               @PathVariable Long paymentTemporaryId,
+                                                                               @RequestBody PaymentConfirmRequest request) {
+        PaymentReservationResponse response = paymentFacade.confirmPaymentByReservation(
+                request.toServiceRequest(paymentTemporaryId, memberId));
+
+        return ApiResponse.ok(response);
     }
 }
--- src/test/java/airdnb/be/web/payment/PaymentControllerTest.java
+++ src/test/java/airdnb/be/web/payment/PaymentControllerTest.java
@@ -9,6 +9,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import airdnb.be.ControllerTestSupport;
+import airdnb.be.domain.payment.service.response.PaymentConfirmResponse;
+import airdnb.be.domain.payment.service.response.PaymentReservationResponse;
+import airdnb.be.domain.reservation.service.response.ReservationResponse;
+import airdnb.be.web.payment.request.PaymentConfirmRequest;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -38,5 +44,62 @@ class PaymentControllerTest extends ControllerTestSupport {
                 .andExpect(jsonPath("$.status").value("OK"))
                 .andExpect(jsonPath("$.message").value("OK"))
                 .andExpect(jsonPath("$.data").exists());
+    }
+
+    @DisplayName("예약 결제 승인에 대해 OK_200 을 응답한다.")
+    @Test
+    void confirmPaymentByReservation() throws Exception {
+        // given
+        Long paymentTemporaryId = 1L;
+        String paymentKey = "paymentKey_123xth43";
+        String amount = "50000";
+        String orderId = "orderId_12323xth43";
+
+        PaymentConfirmRequest request = createPaymentConfirmRequest(paymentKey, amount, orderId);
+
+        PaymentReservationResponse response = createPaymentReservationResponse(paymentKey, amount, orderId);
+        given(paymentFacade.confirmPaymentByReservation(any()))
+                .willReturn(response);
+
+        // when then
+        mockMvc.perform(
+                        post("/payment/{paymentTemporaryId}/confirm", paymentTemporaryId)
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .sessionAttr(LOGIN_MEMBER, 1L))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("0200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("OK"))
+                .andExpect(jsonPath("$.data").exists());
+    }
+
+    private PaymentConfirmRequest createPaymentConfirmRequest(String paymentKey, String amount, String orderId) {
+        return new PaymentConfirmRequest(
+                1L,
+                paymentKey,
+                amount,
+                orderId
+        );
+    }
+
+    private PaymentReservationResponse createPaymentReservationResponse(String paymentKey, String amount, String orderId) {
+        return new PaymentReservationResponse(
+                new PaymentConfirmResponse(
+                        1L,
+                        orderId,
+                        "orderName_예약 1건",
+                        "requestedAt_결제요청시간"
+                ),
+                new ReservationResponse(
+                        1L,
+                        1L,
+                        1L,
+                        LocalDateTime.of(2024, 8, 13, 15, 0),
+                        LocalDateTime.of(2024, 8, 15, 11, 0),
+                        new BigDecimal(amount),
+                        3
+                ));
     }
 }
