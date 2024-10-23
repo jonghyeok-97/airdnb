55ec4663bbc71e1e448e0fc0d8fd4bc1b5341f66
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-10-23 17:41:39 +0900

    [feat/#35] refactor: 테스트 용이성을 위한 API Call을 HttpRequest에서  RestTemplate 으로 변경

--- src/main/java/airdnb/be/client/TossClient.java
+++ src/main/java/airdnb/be/client/TossClient.java
@@ -1,18 +1,17 @@
 package airdnb.be.client;
 
 import airdnb.be.domain.payment.entity.TossPaymentConfirm;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,32 +19,29 @@ import org.springframework.stereotype.Component;
 public class TossClient {
 
     private final Environment env;
-    private final ObjectMapper objectMapper;
 
     public TossPaymentConfirm confirmPayment(String paymentKey, String orderId, String amount)
             throws IOException, InterruptedException {
         String secretApiKey = env.getProperty("payment.toss.api.secret");
         String encodedKey = Base64.getEncoder().encodeToString(secretApiKey.getBytes(StandardCharsets.UTF_8));
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.tosspayments.com/v1/payments/confirm"))
-                .header("Authorization", "Basic " + encodedKey)
-                .header("Content-Type", "application/json")
-                .method("POST", HttpRequest.BodyPublishers.ofString(toJsonPayload(paymentKey, orderId, amount)))
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Basic " + encodedKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<?> httpEntity = new HttpEntity<>(toJsonPayload(paymentKey, orderId, amount), headers);
 
-        return objectMapper.readValue(response.body(), TossPaymentConfirm.class);
+        return restTemplate.postForEntity(
+                "https://api.tosspayments.com/v1/payments/confirm",
+                httpEntity,
+                TossPaymentConfirm.class
+        ).getBody();
     }
 
     private String toJsonPayload(String paymentKey, String orderId, String amount) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("{")
-                .append("\"paymentKey\":\"").append(paymentKey).append("\",")
-                .append("\"orderId\":\"").append(orderId).append("\",")
-                .append("\"amount\":\"").append(amount).append("\"")
-                .append("}");
-
-        return builder.toString();
+        return String.format(
+                "{\"paymentKey\":\"%s\",\"orderId\":\"%s\",\"amount\":\"%s\"}",
+                paymentKey, orderId, amount
+        );
     }
 }
