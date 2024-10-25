e0d0bdbfc142316cf57dd12c13bf243852d6cc40
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-10-25 23:55:14 +0900

    [feat/#42] feat: 결제 및 예약 INSERT 트랜잭션 실패 시 결제 취소 보내는 테스트 및 구현 추가

--- src/test/java/airdnb/be/domain/payment/service/PaymentFacadeTest.java
+++ src/test/java/airdnb/be/domain/payment/service/PaymentFacadeTest.java
@@ -1,14 +1,18 @@
 package airdnb.be.domain.payment.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.BDDMockito.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.times;
 
 import airdnb.be.IntegrationTestSupport;
 import airdnb.be.client.TossClient;
 import airdnb.be.domain.payment.PaymentTemporaryRepository;
-import airdnb.be.domain.payment.TossPaymentConfirmRepository;
 import airdnb.be.domain.payment.entity.PaymentTemporary;
 import airdnb.be.domain.payment.entity.TossPaymentConfirm;
 import airdnb.be.domain.payment.entity.TossPaymentStatus;
@@ -17,13 +21,10 @@ import airdnb.be.domain.payment.service.response.PaymentConfirmResponse;
 import airdnb.be.domain.payment.service.response.PaymentReservationResponse;
 import airdnb.be.domain.reservation.ReservationRepository;
 import airdnb.be.domain.reservation.entity.Reservation;
-import airdnb.be.domain.reservation.service.ReservationService;
-import airdnb.be.domain.reservation.service.ReservationServiceV2;
 import airdnb.be.domain.reservation.service.response.ReservationResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,17 +39,11 @@ class PaymentFacadeTest extends IntegrationTestSupport {
     @MockBean
     private TossClient tossClient;
 
-    @Autowired
-    private PaymentService paymentService;
-
     @MockBean
-    private ReservationServiceV2 reservationServiceV2;
+    private PaymentService paymentService;
 
     @Autowired
     private PaymentTemporaryRepository paymentTemporaryRepository;
-
-    @Autowired
-    private TossPaymentConfirmRepository tossPaymentConfirmRepository;
 
     @Autowired
     private ReservationRepository reservationRepository;
@@ -75,14 +70,7 @@ class PaymentFacadeTest extends IntegrationTestSupport {
         Reservation reservation = saveReservation(50000);
         PaymentTemporary paymentTemporary = savePaymentTemporary("orderId", "paymentKey", "50000");
 
-        PaymentConfirmServiceRequest request = new PaymentConfirmServiceRequest(
-                paymentTemporary.getPaymentTemporaryId(),
-                1L,
-                1L,
-                "paymentKey",
-                "50000",
-                "orderId"
-        );
+        PaymentConfirmServiceRequest request = createPaymentConfirmServiceRequest(paymentTemporary.getPaymentTemporaryId());
 
         TossPaymentConfirm tossPaymentConfirm = TossPaymentConfirm.builder()
                 .paymentKey("paymentKey")
@@ -116,43 +104,35 @@ class PaymentFacadeTest extends IntegrationTestSupport {
     }
 
     @Transactional
-    @DisplayName("결제 승인을 받으면 결제승인과 예약에 대한 응답이 온다")
+    @DisplayName("결제 및 예약 트랜잭션이 실패하면 결제 취소요청을 보낸다")
     @Test
     void confirmPaymentByReservation1() throws JsonProcessingException {
         // given
-        Reservation reservation = saveReservation(50000);
-        PaymentTemporary paymentTemporary = savePaymentTemporary("orderId", "paymentKey", "50000");
+        PaymentConfirmServiceRequest request = createPaymentConfirmServiceRequest(1L);
 
-        PaymentConfirmServiceRequest request = new PaymentConfirmServiceRequest(
-                paymentTemporary.getPaymentTemporaryId(),
+        given(paymentService.confirmReservation(any(), anyLong(), anyLong()))
+                .willThrow(RuntimeException.class);
+        willDoNothing()
+                .given(paymentService)
+                .validateExistingPaymentTemporary(any());
+
+        // when
+        assertThatThrownBy(() -> paymentFacade.confirmPaymentByReservation(request))
+                .isInstanceOf(RuntimeException.class);
+
+        // then
+        verify(tossClient, times(1)).cancelPayment();
+    }
+
+    private PaymentConfirmServiceRequest createPaymentConfirmServiceRequest(Long paymentTemporaryId) {
+        return new PaymentConfirmServiceRequest(
+                paymentTemporaryId,
                 1L,
                 1L,
                 "paymentKey",
                 "50000",
                 "orderId"
         );
-
-        TossPaymentConfirm tossPaymentConfirm = TossPaymentConfirm.builder()
-                .paymentKey("paymentKey")
-                .orderId("orderId")
-                .orderName("orderName")
-                .mId("mId")
-                .lastTransactionKey("lastTransactionKey")
-                .taxExemptionAmount(50000)
-                .status(TossPaymentStatus.DONE)
-                .requestedAt("requestedAt")
-                .build();
-        given(tossClient.confirmPayment(anyString(), anyString(), anyString()))
-                .willReturn(tossPaymentConfirm);
-        given(reservationServiceV2.confirmReservation(anyLong(), anyLong()))
-                .willThrow(new RuntimeException());
-
-        // when
-        PaymentReservationResponse response = paymentFacade.confirmPaymentByReservation(request);
-
-        // then
-        List<TossPaymentConfirm> all = tossPaymentConfirmRepository.findAll();
-        assertThat(all.size()).isEqualTo(0);
     }
 
     private Reservation saveReservation(int amount) {
