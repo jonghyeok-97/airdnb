22d8d3a9f191b55777d424c60a28ad25be82e4ce
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-11-03 21:10:46 +0900

    [feat/#44] refactor: 재시도 실패 시 Recover를 통한 실패 원인 로그 추가

--- src/main/java/airdnb/be/client/MailClient.java
+++ src/main/java/airdnb/be/client/MailClient.java
@@ -3,10 +3,12 @@ package airdnb.be.client;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
+import org.springframework.mail.MailAuthenticationException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.retry.support.RetrySynchronizationManager;
 import org.springframework.scheduling.annotation.Async;
@@ -19,6 +21,7 @@ public class MailClient {
 
     private static final String MAIL_SENDER = "spring.mail.username";
     private static final String TITLE = "Airdnb 클론 프로젝트에서 보낸 인증번호 입니다";
+    private static final int RETRY_MAX_ATTEMPTS = 3;
 
     private final Environment env;
     private final JavaMailSender javaMailSender;
@@ -27,13 +30,18 @@ public class MailClient {
      * @apiNote  메일 예외 발생 시, 1초마다 총 3회 재시도
      */
     @Async("mailExecutor")
-    @Retryable(retryFor = MailSendException.class, backoff = @Backoff(delay = 1000))
+    @Retryable(retryFor = {MailSendException.class}, backoff = @Backoff(delay = 1000))
     public void sendAsyncAuthenticationMail(String toEmail, String authenticationCode) {
         int retryCount = RetrySynchronizationManager.getContext().getRetryCount();
-        log.info("[메일] 메일 전송={}, 시도 횟수 {}/{}", toEmail, ++retryCount, 3);
+        log.info("[메일] 메일 전송={}, 시도 횟수 {}/{}", toEmail, ++retryCount, RETRY_MAX_ATTEMPTS);
 
         SimpleMailMessage mail = createMailMessage(toEmail, authenticationCode);
         javaMailSender.send(mail);
+    }
+
+    @Recover
+    private void occurMailServerError(MailSendException ex) {
+        log.error("[메일] 메일 서버에 메일을 보내는 데 실패했습니다. 예외 메시지: {}", ex.getMessage(), ex);
     }
 
     private SimpleMailMessage createMailMessage(String memberEmail, String randomUUID) {
