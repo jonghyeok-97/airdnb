4397733e5988fd20c0e43604a7645358cbc3ce04
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-11-04 00:11:16 +0900

    [feat/#44] refactor: 재시도 복구 조건을 메일 최상위예외인 MailException으로 변경

--- src/main/java/airdnb/be/client/MailClient.java
+++ src/main/java/airdnb/be/client/MailClient.java
@@ -3,7 +3,7 @@ package airdnb.be.client;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
-import org.springframework.mail.MailAuthenticationException;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -27,22 +27,24 @@ public class MailClient {
     private final JavaMailSender javaMailSender;
 
     /**
-     * @apiNote  메일 예외 발생 시, 1초마다 총 3회 재시도
+     * @apiNote 메일 예외 발생 시, 1초마다 총 3회 재시도
      */
     @Async("mailExecutor")
     @Retryable(retryFor = {MailSendException.class}, backoff = @Backoff(delay = 1000))
     public void sendAsyncAuthenticationMail(String toEmail, String authenticationCode) {
         int retryCount = RetrySynchronizationManager.getContext().getRetryCount();
-        log.info("[메일] 메일 전송={}, 시도 횟수 {}/{}", toEmail, ++retryCount, RETRY_MAX_ATTEMPTS);
+        log.info("[메일 전송 시도] toEmail={}, 시도 횟수={}/{}", toEmail, ++retryCount, RETRY_MAX_ATTEMPTS);
 
         SimpleMailMessage mail = createMailMessage(toEmail, authenticationCode);
         javaMailSender.send(mail);
+        log.info("[메일 전송 완료] toEmail={}", toEmail);
     }
 
     @Recover
-    private void occurMailServerError(MailSendException ex) {
-        log.error("[메일] 메일 서버에 메일을 보내는 데 실패했습니다. 예외 메시지: {}", ex.getMessage(), ex);
+    public void recoverSendAsyncAuthenticationMail(MailException ex) {
+        log.error("[메일 전송 실패] {}", ex.getMessage(), ex);
     }
+
 
     private SimpleMailMessage createMailMessage(String memberEmail, String randomUUID) {
         SimpleMailMessage message = new SimpleMailMessage();
