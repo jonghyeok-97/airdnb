b0eaa279bfe3c64c9600ebaf1940428b0ba7e793
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-11-05 19:15:19 +0900

    [feat/#46] feat: RestTemplate 에 타임아웃 적용

--- src/main/java/airdnb/be/client/TossClient.java
+++ src/main/java/airdnb/be/client/TossClient.java
@@ -4,9 +4,9 @@ import airdnb.be.domain.payment.entity.TossPaymentConfirm;
 import airdnb.be.domain.payment.toss.TossPaymentDto;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class TossClient {
 
@@ -21,10 +22,6 @@ public class TossClient {
     private String secretApiKey;
 
     private final RestTemplate restTemplate;
-
-    public TossClient(RestTemplateBuilder builder) {
-        this.restTemplate = builder.build();
-    }
 
     public TossPaymentConfirm confirmPayment(String paymentKey, String orderId, String amount) {
         HttpHeaders headers = new HttpHeaders();
