7cfcbe646932bd89f77429202a756eac18108246
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-12-04 13:33:27 +0900

    [feat/#49] test: 테스트용 임베디드 레디스 설정 변경

--- src/test/java/airdnb/be/config/EmbeddedRedisConfig.java
+++ src/test/java/airdnb/be/config/EmbeddedRedisConfig.java
@@ -1,82 +1,85 @@
-//package airdnb.be.config;
-//
-//import airdnb.be.IntegrationTestSupport;
-//import jakarta.annotation.PostConstruct;
-//import jakarta.annotation.PreDestroy;
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.util.StringUtils;
-//import redis.embedded.RedisServer;
-//
-//@Profile("test")
-//@Configuration
-//public class EmbeddedRedisConfig {
-//
-//    private final int port = 6379;
-//
-//    private RedisServer redisServer;
-//
+package airdnb.be.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.TestConfiguration;
+import redis.embedded.RedisServer;
+
+@Slf4j
+@TestConfiguration
+public class EmbeddedRedisConfig {
+
+    private final int port = 6378;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() throws IOException {
+        redisServer = new RedisServer(port);
+        try {
+            redisServer.start();
+        } catch (Exception e) {
+            log.warn("테스트 실패 시 embedded redis 강제 종료");
+            stopRedis();
+        }
+    }
+
+    @PreDestroy
+    public void stopRedis() throws IOException {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+
+    /**
+     * 병렬로 테스트를 실행하게 되어 여러 레디스가 띄워질 때, 주석을 해제하기
+     */
 //    @PostConstruct
-//    public void redisServer() throws IOException {
-//        redisServer = new RedisServer(port);
+//    public void postConstruct() throws IOException {
+//        int port = isRedisRunning() ? findAvaliablePort() : this.port;
+//        this.redisServer = new RedisServer(port);
 //        redisServer.start();
 //    }
 //
 //    @PreDestroy
-//    public void stopRedis() throws IOException {
+//    public void preDestroy() throws IOException {
 //        if (redisServer != null) {
 //            redisServer.stop();
 //        }
 //    }
 //
-////    @PostConstruct
-////    public void postConstruct() throws IOException {
-////        int port = isRedisRunning() ? findAvaliablePort() : this.port;
-////        this.redisServer = new RedisServer(port);
-////        redisServer.start();
-////    }
-////
-////    @PreDestroy
-////    public void preDestroy() throws IOException {
-////        if (redisServer != null) {
-////            redisServer.stop();
-////        }
-////    }
-////
-////    private int findAvaliablePort() throws IOException{
-////        for (int port = 10000; port <= 65535; port++) {
-////            Process process = executeGrepProcessCommand(port);
-////            if (!isRunning(process)) {
-////                return port;
-////            }
-////        }
-////        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
-////    }
-////
-////    private boolean isRedisRunning() throws IOException{
-////        return isRunning(executeGrepProcessCommand(this.port));
-////    }
-////
-////    private boolean isRunning(Process process) {
-////        String line;
-////        StringBuilder pidInfo = new StringBuilder();
-////
-////        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-////            while ((line = input.readLine()) != null) {
-////                pidInfo.append(line);
-////            }
-////        } catch (Exception e) {}
-////
-////        return StringUtils.hasLength(pidInfo.toString());
-////    }
-////
-////    private Process executeGrepProcessCommand(int port) throws IOException {
-////        String command = String.format("netstat -nao | find \"LISTEN\" | find \"%d\"", port);
-////        String[] shell = {"cmd.exe", "/y", "/c", command};
-////        return Runtime.getRuntime().exec(shell);
-////    }
-//}
+//    private int findAvaliablePort() throws IOException{
+//        for (int port = 10000; port <= 65535; port++) {
+//            Process process = executeGrepProcessCommand(port);
+//            if (!isRunning(process)) {
+//                return port;
+//            }
+//        }
+//        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+//    }
+//
+//    private boolean isRedisRunning() throws IOException{
+//        return isRunning(executeGrepProcessCommand(this.port));
+//    }
+//
+//    private boolean isRunning(Process process) {
+//        String line;
+//        StringBuilder pidInfo = new StringBuilder();
+//
+//        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+//            while ((line = input.readLine()) != null) {
+//                pidInfo.append(line);
+//            }
+//        } catch (Exception e) {}
+//
+//        return StringUtils.hasLength(pidInfo.toString());
+//    }
+//
+//    private Process executeGrepProcessCommand(int port) throws IOException {
+//        String command = String.format("netstat -nao | find \"LISTEN\" | find \"%d\"", port);
+//        String[] shell = {"cmd.exe", "/y", "/c", command};
+//        return Runtime.getRuntime().exec(shell);
+//    }
+}
